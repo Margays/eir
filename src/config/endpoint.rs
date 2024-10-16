19f9ee0c6c72430767893a4e38c3127891389c9d
Lukasz Wencel <65486779+lwencel-priv@users.noreply.github.com>
2024-10-16 17:33:11 +0200

    run in parallel

--- src/config/endpoint.rs
+++ src/config/endpoint.rs
@@ -5,5 +5,6 @@ use super::metric::Metric;
 #[derive(Deserialize, Debug)]
 pub struct Endpoint {
     pub url: String,
+    pub interval: String,
     pub metrics: Vec<Metric>,
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use jsonpath_rust::JsonPath;
 use metrics::init_metrics;
 use serde_json::Value;
 use std::str::FromStr;
+use std::thread;
 
 mod client;
 mod config;
@@ -26,21 +27,41 @@ fn resolve_labels(labels: &Vec<Label>, response: &Value) -> Vec<(String, String)
     resolved_labels
 }
 
+fn parse_interval(interval: &str) -> u64 {
+    let mut value = interval.to_string();
+    let unit = value.pop().unwrap();
+    let value = value.parse::<u64>().unwrap();
+    match unit {
+        's' => value,
+        'm' => value * 60,
+        'h' => value * 60 * 60,
+        _ => panic!("Invalid interval unit"),
+    }
+}
+
 fn main() {
     let config = config::load_config();
     init_metrics(&3000, &config);
 
     let api_client = client::http::HttpClient::new(&config.client.headers);
-    for endpoint in &config.endpoints {
-        for metric in &endpoint.metrics {
-            let response = api_client.get(endpoint.url.as_str()).unwrap();
-            let path = JsonPath::from_str(metric.json_path.as_str()).unwrap();
-            let val = path.find_slice(&response);
-            let value = val[0].clone().to_data().as_f64().unwrap();
-            let labels = resolve_labels(&metric.labels.clone(), &response);
-            let gauge = gauge!(metric.name.clone(), &labels);
-            gauge.set(value);
+    thread::scope(|s| {
+        for endpoint in &config.endpoints {
+            s.spawn(|| {
+                loop {
+                    let response = api_client.get(endpoint.url.as_str()).unwrap();
+                    for metric in &endpoint.metrics {
+                        let path = JsonPath::from_str(metric.json_path.as_str()).unwrap();
+                        let val = path.find_slice(&response);
+                        let value = val[0].clone().to_data().as_f64().unwrap();
+                        let labels = resolve_labels(&metric.labels.clone(), &response);
+                        let gauge = gauge!(metric.name.clone(), &labels);
+                        gauge.set(value);
+                    }
+                    let interval = parse_interval(endpoint.interval.as_str());
+                    thread::sleep(std::time::Duration::from_secs(interval));
+                    println!("Sleeping for {} seconds", interval);
+                }
+            });
         }
-    }
-    std::thread::sleep(std::time::Duration::from_secs(60));
+    });
 }
