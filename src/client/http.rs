2a88405562a30bc911f8f3f8aaeccf01ef90c87c
Łukasz Wencel <65486779+lwencel-priv@users.noreply.github.com>
2025-07-17 22:27:16 +0200

    Fix JSON formatting and enhance error handling in HTTP client (#12)

--- src/client/http.rs
+++ src/client/http.rs
@@ -1,6 +1,7 @@
 use super::Client;
 use reqwest::header::HeaderMap;
 use serde_json::{Value, json};
+use std::fmt::Display;
 use std::str::FromStr;
 use std::sync::Arc;
 use std::{collections::HashMap, error::Error};
@@ -37,6 +38,15 @@ impl Client for HttpClient {
         let permit = self.semaphore.acquire().await.unwrap();
         let response = request.send().await?;
         drop(permit);
+        let status = response.status();
+        if status != reqwest::StatusCode::OK {
+            let text = response.text().await?;
+            return Err(Box::new(RequestError {
+                url: url.to_string(),
+                status,
+                message: format!("Request failed with status: {status}, response: {text}"),
+            }));
+        }
         let headers: Value = response
             .headers()
             .iter()
@@ -46,12 +56,33 @@ impl Client for HttpClient {
         let result: Value = json!({
             "response": {
                 "headers": headers,
-                "json": json_data
+                "json": json_data,
+                "status": status.as_u16(),
             }
         });
         Ok(result)
     }
 }
+
+// Define request error for better error handling
+#[derive(Debug)]
+pub struct RequestError {
+    pub url: String,
+    pub status: reqwest::StatusCode,
+    pub message: String,
+}
+
+impl Display for RequestError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Request to {} failed with status {}: {}",
+            self.url, self.status, self.message
+        )
+    }
+}
+
+impl std::error::Error for RequestError {}
 
 #[cfg(test)]
 mod tests {
